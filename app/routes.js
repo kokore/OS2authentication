a463bdaf71d0f3257e54a21ba75ba3a6ffa3ef62
kokore <kokore_za@hotmail.com>
2016-10-24 16:41:12 +0700

    new work

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,8 @@
 module.exports = function(app, passport) {
 
-    // =====================================
-    // HOME PAGE (with login links) ========
-    // =====================================
+// normal routes ===============================================================
+
+    // show the home page (will also have our login links)
     app.get('/', function(req, res) {
         res.render('index.ejs');
     });
